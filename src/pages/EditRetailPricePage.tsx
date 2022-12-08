355d884f921e501f3385f3e227d91d123d37eeb8
Aleksander <fardmail@rambler.ru>
2022-12-08 19:06:42 +0300

    edit price page

--- src/pages/EditRetailPricePage.tsx
+++ src/pages/EditRetailPricePage.tsx
@@ -1,20 +1,22 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Button, Card, Form, Table,Container } from "react-bootstrap";
 import { observer } from "mobx-react-lite";
+import { Context } from "..";
+import { getRetailPrice } from "../components/axios/PriceApi";
 
 
 
 
 
 const EditRetailPricePage = observer(() => {
-    
 
+    const {price} = useContext(Context)
 
-    
-   
+    useEffect(()=>{
+        getRetailPrice().then((data) => price.setCurrentPriceList(data))
+    }, [])
 
-    
-    return <Container><div> " 123"</div></Container>
+    return (<Container><div> {price.currentPriceList.vinyl}</div></Container>)
           
     
 });
