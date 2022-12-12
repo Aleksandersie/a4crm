3389363ecd6b80702b905b368adf4aca055228fb
Aleksandersie <aleksander.sie@gmail.com>
2022-12-12 23:13:05 +0300

    change order cost

--- src/components/OrderPanelString/OrderElementAccordion.tsx
+++ src/components/OrderPanelString/OrderElementAccordion.tsx
@@ -1,19 +1,19 @@
-import React, { useState,useContext } from "react";
-import { Accordion, Button, Card, Table } from "react-bootstrap";
-import { AiFillDelete,AiOutlineSetting } from "react-icons/ai";
+import React, { useState, useContext, useRef } from "react";
+import { Accordion, Button, Card, Modal, Table, Form } from "react-bootstrap";
+import { AiFillDelete, AiOutlineSetting } from "react-icons/ai";
 import { BiRuble } from "react-icons/bi";
 import { IOrderItem } from "../../calcLogic/calc";
 import { NavLink } from "react-router-dom";
 import { MAIN_ROUTE } from "../../routeConst/routeConst";
 import axios from "axios";
-import { downloadFile } from "../axios/OrderApi";
+import { changeOrderCost, downloadFile } from "../axios/OrderApi";
 import userEvent from "@testing-library/user-event";
 import { Context } from "../..";
 import { adminConst } from "../../Const";
 
 interface IOrderAccordion {
-    key: any;
-    orderAccordion: IOrderItem;
+     key: any;
+     orderAccordion: IOrderItem;
 }
 
 // async function download(path) {
@@ -34,116 +34,169 @@ interface IOrderAccordion {
 //         });
 // }
 function download(path) {
-    downloadFile(path);
+     downloadFile(path);
 }
 
 const OrderElementAccordion: React.FC<IOrderAccordion> = ({ orderAccordion }) => {
-const {user} =  useContext(Context)
+     const { user } = useContext(Context);
+     const [showEdit, setShowEdit] = useState<boolean>(false);
+     const newPrice = React.useRef<HTMLInputElement>();
+     function showPriceModal(random: number, e: React.MouseEvent) {
+          e.stopPropagation();
+          setShowEdit(true);
+     }
+     async function editThisPrice(random, e) {
+          e.stopPropagation();
+          console.log(random);
+          console.log(newPrice.current.value);
+          await changeOrderCost(+newPrice.current.value, random);
+     }
 
-function editThisPrice(random){
-    
-    console.log(random);
-    
-}
+     return (
+          <Accordion className="mt-2 shadow">
+               <Accordion.Item eventKey="0">
+                    <Accordion.Header>
+                         <Card
+                              className="  pb-1 pt-2 d-flex flex-row justify-content-center ms-5 me-5 shadow-sm gap-4"
+                              style={{
+                                   minWidth: 300,
+                                   backgroundColor: "whitesmoke",
+                                   width: "100% ",
+                              }}
+                         >
+                              <div style={{ textAlign: "center" }}>
+                                   <h6>{"Категория заказа"}</h6>
+                                   <p>{orderAccordion.orderCategory}</p>
+                              </div>
+                              <div style={{ textAlign: "center" }}>
+                                   <h6>{"Материал"}</h6>
+                                   <p>{orderAccordion.material}</p>
+                              </div>
+                              <div className="" style={{ textAlign: "center" }}>
+                                   <h6>{"Размеры"}</h6>
+                                   <p>
+                                        {orderAccordion.width}x{orderAccordion.height}
+                                   </p>
+                              </div>
+                              <div style={{ textAlign: "center" }}>
+                                   <h6>{"Количество"}</h6>
+                                   <p>{orderAccordion.count}</p>
+                              </div>
+                              {user.user.role === adminConst ? (
+                                   <div style={{ textAlign: "center" }}>
+                                        <h6>{"Редактировать стоимость"}</h6>
+                                        <p>
+                                             {
+                                                  <AiOutlineSetting
+                                                       onClick={(e: React.MouseEvent) =>
+                                                            showPriceModal(orderAccordion.random, e)
+                                                       }
+                                                  />
+                                             }
+                                        </p>
+                                   </div>
+                              ) : (
+                                   ""
+                              )}
+                              <Modal show={showEdit} onHide={() => setShowEdit(false)}>
+                                   <Modal.Header closeButton>
+                                        <Modal.Title>Изменение стоимости</Modal.Title>
+                                   </Modal.Header>
+                                   <Modal.Body>
+                                        <div>
+                                             <p>Новая стоимость</p>
+                                             <Form.Control
+                                                  type={"number"}
+                                                  ref={newPrice}
+                                                  placeholder={String(orderAccordion.totalCost)}
+                                             />
+                                        </div>
+                                   </Modal.Body>
+                                   <Modal.Footer>
+                                        <Button
+                                             variant="secondary"
+                                             onClick={() => setShowEdit(false)}
+                                        >
+                                             Close
+                                        </Button>
+                                        <Button
+                                             variant="primary"
+                                             onClick={(e: React.MouseEvent) =>
+                                                  editThisPrice(orderAccordion.random, e)
+                                             }
+                                        >
+                                             Save Changes
+                                        </Button>
+                                   </Modal.Footer>
+                              </Modal>
 
-    return (
-        <Accordion className="mt-2 shadow">
-            <Accordion.Item eventKey="0">
-                <Accordion.Header>
-                    <Card
-                        className="  pb-1 pt-2 d-flex flex-row justify-content-center ms-5 me-5 shadow-sm gap-4"
-                        style={{
-                            minWidth: 300,
-                            backgroundColor: "whitesmoke",
-                            width: "100% ",
-                        }}
-                    >
-                        <div style={{ textAlign: "center" }}>
-                            <h6>{"Категория заказа"}</h6>
-                            <p>{orderAccordion.orderCategory}</p>
-                        </div>
-                        <div style={{ textAlign: "center" }}>
-                            <h6>{"Материал"}</h6>
-                            <p>{orderAccordion.material}</p>
-                        </div>
-                        <div className="" style={{ textAlign: "center" }}>
-                            <h6>{"Размеры"}</h6>
-                            <p>
-                                {orderAccordion.width}x{orderAccordion.height}
-                            </p>
-                        </div>
-                        <div style={{ textAlign: "center" }}>
-                            <h6>{"Количество"}</h6>
-                            <p>{orderAccordion.count}</p>
-                        </div>
-                        {user.user.role===adminConst? <div style={{ textAlign: "center" }}>
-                            <h6>{"Редактировать стоимость"}</h6>
-                            <p>{<AiOutlineSetting onClick={()=>editThisPrice(orderAccordion.random)}/>}</p>
-                        </div>:""}
-                        
-                        {/*<div style={{ textAlign: "center" }}>*/}
-                        {/*    <h6>{"Удалить"}</h6>*/}
-                        {/*    <AiFillDelete*/}
-                        {/*        style={{ fontSize: 22 }}*/}
-                        {/*        onClick={() => removeItem(orderItem.random)}*/}
-                        {/*    />*/}
-                        {/*</div>*/}
-                    </Card>
-                </Accordion.Header>
-                <Accordion.Body>
-                    <Table striped bordered hover size="sm" className="mt-2">
-                        <thead>
-                            <tr>
-                                <th colSpan={4}>
-                                    Размеры изделия: {orderAccordion.width}x{orderAccordion.height}{" "}
-                                    м. Количество: {orderAccordion.count} шт. <br /> Материал:{" "}
-                                    {orderAccordion.material}
-                                </th>
-                            </tr>
-                            <tr>
-                                <th>Общая площадь:</th>
-                                <th>Площадь одной штуки:</th>
-                                <th>Стоимость одной штуки:</th>
-                                <th>Общая стоимость:</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{orderAccordion.totalArea}</td>
-                                <td>{orderAccordion.onePcsArea}</td>
-                                <td>
-                                    {orderAccordion.onePcsCost} <BiRuble />
-                                </td>
-                                <td>
-                                    {orderAccordion.totalCost} <BiRuble />
-                                </td>
-                            </tr>
-                        </tbody>
-                        <thead>
-                            <tr>
-                                <th colSpan={2}>Описание заказа</th>
+                              {/*<div style={{ textAlign: "center" }}>*/}
+                              {/*    <h6>{"Удалить"}</h6>*/}
+                              {/*    <AiFillDelete*/}
+                              {/*        style={{ fontSize: 22 }}*/}
+                              {/*        onClick={() => removeItem(orderItem.random)}*/}
+                              {/*    />*/}
+                              {/*</div>*/}
+                         </Card>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                         <Table striped bordered hover size="sm" className="mt-2">
+                              <thead>
+                                   <tr>
+                                        <th colSpan={4}>
+                                             Размеры изделия: {orderAccordion.width}x
+                                             {orderAccordion.height} м. Количество:{" "}
+                                             {orderAccordion.count} шт. <br /> Материал:{" "}
+                                             {orderAccordion.material}
+                                        </th>
+                                   </tr>
+                                   <tr>
+                                        <th>Общая площадь:</th>
+                                        <th>Площадь одной штуки:</th>
+                                        <th>Стоимость одной штуки:</th>
+                                        <th>Общая стоимость:</th>
+                                   </tr>
+                              </thead>
+                              <tbody>
+                                   <tr>
+                                        <td>{orderAccordion.totalArea}</td>
+                                        <td>{orderAccordion.onePcsArea}</td>
+                                        <td>
+                                             {orderAccordion.onePcsCost} <BiRuble />
+                                        </td>
+                                        <td>
+                                             {orderAccordion.totalCost} <BiRuble />
+                                        </td>
+                                   </tr>
+                              </tbody>
+                              <thead>
+                                   <tr>
+                                        <th colSpan={2}>Описание заказа</th>
 
-                                <th>Ламинация:</th>
-                                <th>Подрезка:</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td colSpan={2}></td>
+                                        <th>Ламинация:</th>
+                                        <th>Подрезка:</th>
+                                   </tr>
+                              </thead>
+                              <tbody>
+                                   <tr>
+                                        <td colSpan={2}>{orderAccordion.description}</td>
 
-                                <td>{orderAccordion.lamination ? "Да" : "Нет"} </td>
-                                <td>{orderAccordion.borderCut ? "Да" : "Нет"} </td>
-                            </tr>
-                        </tbody>
-                    </Table>
+                                        <td>{orderAccordion.lamination ? "Да" : "Нет"} </td>
+                                        <td>{orderAccordion.borderCut ? "Да" : "Нет"} </td>
+                                   </tr>
+                              </tbody>
+                         </Table>
 
-                    <Button variant={"warning"} onClick={() => download(orderAccordion.filePath)}>
-                        Скачать файл
-                    </Button>
-                </Accordion.Body>
-            </Accordion.Item>
-        </Accordion>
-    );
+                         <Button
+                              variant={"warning"}
+                              onClick={() => download(orderAccordion.filePath)}
+                         >
+                              Скачать файл
+                         </Button>
+                    </Accordion.Body>
+               </Accordion.Item>
+          </Accordion>
+     );
 };
 
 export default OrderElementAccordion;
--- src/components/axios/OrderApi.ts
+++ src/components/axios/OrderApi.ts
@@ -3,80 +3,84 @@ import { IOrderItem } from "../../calcLogic/calc";
 import axios from "axios";
 
 export interface IIncomingOrder {
-    id: number;
-    randomNumber: number;
-    owner: string;
-    author: string;
-    createdDate: string;
-    orderStatus: string;
-    orderTotalCost: number;
-    orderMessage: string;
-    orderPaid: boolean;
-    orderItems: IOrderItem[];
+     id: number;
+     randomNumber: number;
+     owner: string;
+     author: string;
+     createdDate: string;
+     orderStatus: string;
+     orderTotalCost: number;
+     orderMessage: string;
+     orderPaid: boolean;
+     orderItems: IOrderItem[];
 }
 
 export const createOrder = async function (data: IOrderItem[], orderMessage: string) {
-    const res = await host.post("api/order", { data, orderMessage });
+     const res = await host.post("api/order", { data, orderMessage });
 };
 
 export const uploadFile = async function (formData) {
-    const res = await host.post("api/order/file", formData, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
-    });
-    return res;
+     const res = await host.post("api/order/file", formData, {
+          headers: {
+               "Content-Type": "multipart/form-data",
+          },
+     });
+     return res;
 };
 
 export const downloadFile = async function (path) {
-    const fileName = path.split("\\");
-    //const fileName = path.split("/"); //для сервера
+     const fileName = path.split("\\");
+     //const fileName = path.split("/"); //для сервера
 
-    console.log(fileName);
-    const res = await host
-        .get("api/order/download", { responseType: "blob", params: { path } })
-        .then((response) => {
-            const href = URL.createObjectURL(response.data);
-            const link = document.createElement("a");
-            link.href = href;
-            link.setAttribute("download", fileName[fileName.length - 1]);
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-            URL.revokeObjectURL(href);
-        });
+     console.log(fileName);
+     const res = await host
+          .get("api/order/download", { responseType: "blob", params: { path } })
+          .then((response) => {
+               const href = URL.createObjectURL(response.data);
+               const link = document.createElement("a");
+               link.href = href;
+               link.setAttribute("download", fileName[fileName.length - 1]);
+               document.body.appendChild(link);
+               link.click();
+               document.body.removeChild(link);
+               URL.revokeObjectURL(href);
+          });
 };
 export const getAllOrders = async function (page: number, limit: number) {
-    const response = await host.get<IIncomingOrder[]>("api/order", {
-        params: {
-            page,
-            limit,
-        },
-    });
-    return response.data;
+     const response = await host.get<IIncomingOrder[]>("api/order", {
+          params: {
+               page,
+               limit,
+          },
+     });
+     return response.data;
 };
 
 export const sendOrderStatus = async function (status: string, randomNumber: number) {
-    const res = await host.put("api/order/changeStatus", { status, randomNumber });
-    return res.data;
+     const res = await host.put("api/order/changeStatus", { status, randomNumber });
+     return res.data;
 };
 export const searchOrder = async function (limit: number, page: number, word: string) {
-    const res = await host.get("api/order/searchOrder", {
-        params: {
-            limit,
-            page,
-            word,
-        },
-    });
-    return res.data;
+     const res = await host.get("api/order/searchOrder", {
+          params: {
+               limit,
+               page,
+               word,
+          },
+     });
+     return res.data;
 };
 export const getOrdersByFilter = async function (limit: number, page: number, filter: string) {
-    const res = await host.get("api/order/getOrdersByFilter", {
-        params: {
-            limit,
-            page,
-            filter,
-        },
-    });
-    return res.data;
+     const res = await host.get("api/order/getOrdersByFilter", {
+          params: {
+               limit,
+               page,
+               filter,
+          },
+     });
+     return res.data;
+};
+export const changeOrderCost = async function (newPrice: number, random: number) {
+     const res = await host.put("api/order/changeOrderPrice", { newPrice, random });
+     return res.data;
 };
