c83727b27abd5044a519d41f59fce657bc6d2cf5
Aleksandersie <aleksander.sie@gmail.com>
2022-12-18 12:03:35 +0300

    search owner

--- src/components/CalcBlock/CaclBlock.tsx
+++ src/components/CalcBlock/CaclBlock.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Button, Card, Dropdown,Modal } from "react-bootstrap";
+import { Button, Card, Dropdown, Modal } from "react-bootstrap";
 import CalcInputBlock from "../CalcInputBlock/CalcInputBlock";
 import { Paper } from "@mui/material";
 import { AiOutlineArrowLeft } from "react-icons/ai";
@@ -10,88 +10,92 @@ import MaterialDropdown from "../MaterialDropdown/MaterialDropdown";
 import TypeDropdown from "../TypeDropdown/TypeDropdown";
 import CategoryDropdown from "../CategoryDropdown/CategoryDropdown";
 import CustomersDropdown from "../CustomersDropdown/CustomersDropdown";
-import { adminConst,managerConst } from "../../Const";
+import { adminConst, managerConst } from "../../Const";
 import SearchCustomers from "../SearchCustomers/SearchCustomers";
 
-
 const CalcBlock = observer(() => {
-    const navigate = useNavigate();
-    const { materialList,user } = useContext(Context);
-    const [showFind,setShowFind] = useState(false)
-    const goBack = () => {
-        navigate(-1);
-    };
-    return (
-        <Paper elevation={3} style={{ textAlign: "center" }} className="mt-4">
-            <Card className="text-center ">
-                <Card.Header style={{ fontSize: 25, fontWeight: 500 }}>
-                    <div >
-                        <AiOutlineArrowLeft
-                            style={{
-                                display: "flex",
-                                fontSize: 25,
-                                cursor: "pointer",
-                                position: "absolute",
-                                top: 10,
-                            }}
-                            onClick={goBack}
-                        />
-                        <div> Калькулятор расчета печати</div>
-                    </div>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Subtitle>
-                        {
-                            user.user.role === (adminConst || managerConst) ? 
-                            <div>
+     const navigate = useNavigate();
+     const { materialList, user } = useContext(Context);
+     const [showFind, setShowFind] = useState(false);
+     const goBack = () => {
+          navigate(-1);
+     };
+     return (
+          <Paper elevation={3} style={{ textAlign: "center" }} className="mt-4">
+               <Card className="text-center ">
+                    <Card.Header style={{ fontSize: 25, fontWeight: 500 }}>
+                         <div>
+                              <AiOutlineArrowLeft
+                                   style={{
+                                        display: "flex",
+                                        fontSize: 25,
+                                        cursor: "pointer",
+                                        position: "absolute",
+                                        top: 10,
+                                   }}
+                                   onClick={goBack}
+                              />
+                              <div> Калькулятор расчета печати</div>
+                         </div>
+                    </Card.Header>
+                    <Card.Body>
+                         <Card.Subtitle>
+                              {user.user.role === (adminConst || managerConst) ? (
+                                   <div>
+                                        <div className="mb-2">Выберите заказчика:</div>
+                                        <CustomersDropdown />
+                                        <Button variant="warning" onClick={() => setShowFind(true)}>
+                                             Выбрать заказчика
+                                        </Button>
 
-                            <div className="mb-2">Выберите заказчика:</div>
-                            <CustomersDropdown />
-                            <Button variant="warning" onClick={()=>setShowFind(true)}>Выбрать заказчика</Button>
+                                        <Modal show={showFind} onHide={() => setShowFind(false)}>
+                                             <Modal.Header closeButton>
+                                                  <Modal.Title>Выберите заказчика</Modal.Title>
+                                             </Modal.Header>
+                                             <Modal.Body>
+                                                  <SearchCustomers show={setShowFind} />
+                                             </Modal.Body>
+                                             <Modal.Footer>
+                                                  <Button
+                                                       variant="secondary"
+                                                       onClick={() => setShowFind(false)}
+                                                  >
+                                                       Close
+                                                  </Button>
+                                                  <Button
+                                                       variant="primary"
+                                                       onClick={() => setShowFind(false)}
+                                                  >
+                                                       Save Changes
+                                                  </Button>
+                                             </Modal.Footer>
+                                        </Modal>
+                                   </div>
+                              ) : (
+                                   ""
+                              )}
 
-                            <Modal show={showFind} onHide={()=>setShowFind(false)}>
-                             <Modal.Header closeButton>
-                               <Modal.Title>Выберите заказчика</Modal.Title>
-                             </Modal.Header>
-                             <Modal.Body>
-                                <SearchCustomers/>
-                                </Modal.Body>
-                             <Modal.Footer>
-                               <Button variant="secondary" onClick={()=>setShowFind(false)}>
-                                 Close
-                               </Button>
-                               <Button variant="primary" onClick={()=>setShowFind(false)}>
-                                 Save Changes
-                               </Button>
-                             </Modal.Footer>
-                           </Modal>
-                            </div>
-                           
-                            :
-                            ""
-                        }
-                        
-                        <div className="d-flex justify-content-center align-items-center gap-5 mt-2">
-                            <div>
-                                <div className="mb-2">Категория заказа:</div>
-                                <CategoryDropdown />
-                            </div>
-                            <div>
-                                <div className="mb-2">Категория материала:</div>
-                                <TypeDropdown />
-                            </div>
-                            <div>
-                                <div className="mb-2">Используемый материал: </div>
-                                <MaterialDropdown />
-                            </div>
-                        </div>
-                    </Card.Subtitle>
-                    <Card.Title style={{ marginTop: 30 }}>Введите размеры:</Card.Title>
-                    <CalcInputBlock />
-                </Card.Body>
-            </Card>
-        </Paper>
-    );
+                              <div className="d-flex justify-content-center align-items-center gap-5 mt-2">
+                                   <div>
+                                        <div className="mb-2">Категория заказа:</div>
+                                        <CategoryDropdown />
+                                   </div>
+                                   <div>
+                                        <div className="mb-2">Категория материала:</div>
+                                        <TypeDropdown />
+                                   </div>
+                                   <div>
+                                        <div className="mb-2">Используемый материал: </div>
+                                        <MaterialDropdown />
+                                   </div>
+                              </div>
+                         </Card.Subtitle>
+                         <Card.Title style={{ marginTop: 30 }}>Введите размеры:</Card.Title>
+                         <CalcInputBlock />
+                    </Card.Body>
+               </Card>
+          </Paper>
+     );
 });
 
 export default CalcBlock;
--- src/components/SearchCustomers/SearchCustomers.tsx
+++ src/components/SearchCustomers/SearchCustomers.tsx
@@ -1,12 +1,54 @@
-import { observer } from "mobx-react-lite"
-import { Form } from "react-bootstrap"
+import { observer } from "mobx-react-lite";
+import { Form, Table } from "react-bootstrap";
+import { Button } from "@mui/material";
+import React, { useContext } from "react";
+import { searchUser } from "../axios/UserApi";
+import { Context } from "../../index";
 
+interface ISearchCustomers {
+     show: any;
+}
+const SearchCustomers: React.FC<ISearchCustomers> = observer(({ show }) => {
+     const { user } = useContext(Context);
+     const name = React.useRef<HTMLInputElement>(null);
 
-const SearchCustomers:React.FC = observer(()=>{
+     async function search() {
+          await searchUser(name.current.value)
+               .then((data) => user.setCustomers(data))
+               .finally(() => console.log(user.customers));
+     }
 
-    return <div>
-        <Form.Control/>
-    </div>
-})
+     function setOrderOwner(owner) {
+          user.setSelectedCustomer(owner);
+          show(false);
+     }
 
-export default SearchCustomers
+     return (
+          <div>
+               <div className={"d-flex gap-4 flex-column"}>
+                    <div className={"d-flex gap-4"}>
+                         <Form.Control ref={name} />
+                         <Button variant="contained" onClick={search}>
+                              Найти
+                         </Button>
+                    </div>
+                    <Table striped bordered hover style={{ textAlign: "center" }}>
+                         <tbody>
+                              {user.customers.rows.map((customer) => (
+                                   <tr>
+                                        <td
+                                             onClick={() => setOrderOwner(customer)}
+                                             style={{ cursor: "pointer" }}
+                                        >
+                                             {customer.alias}
+                                        </td>
+                                   </tr>
+                              ))}
+                         </tbody>
+                    </Table>
+               </div>
+          </div>
+     );
+});
+
+export default SearchCustomers;
