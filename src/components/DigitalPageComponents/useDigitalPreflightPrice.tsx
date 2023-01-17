a9c7f985d3b703c563ab75d9d15468753a0295ef
Aleksander <fardmail@rambler.ru>
2023-01-17 12:35:30 +0300

    adr change

--- src/components/DigitalPageComponents/useDigitalPreflightPrice.tsx
+++ src/components/DigitalPageComponents/useDigitalPreflightPrice.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react"
 
-
+//const [value, setValue] = useState('')
 const useDigitalPreflightPrice = (numberOfCopy,price)=>{
-
+    
     const totalPrintSum = numberOfCopy*price
     const onePcsCost = 1 * price
     console.log(totalPrintSum)
--- src/components/DigitalPageComponents/useDigitalPrintPriceSelector.tsx
+++ src/components/DigitalPageComponents/useDigitalPrintPriceSelector.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
+import { Context } from "../..";
 import { digitalCategoryEnum } from "../../Store/DigitalPrintStore"
 
 
 
 const useDigitalPrintPriceSelector=(category)=>{
+    // const {price} = useContext(Context) 
    if(category===digitalCategoryEnum.sheetFeed){
-    console.log("ok");
+    // console.log(price);
     
    }
 }
