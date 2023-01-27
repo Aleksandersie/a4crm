805c545416e61577ecd81faf97c4bd4801eb8f29
Aleksander <fardmail@rambler.ru>
2023-01-27 15:19:32 +0300

    price array

--- src/components/DigitalPageComponents/useStepper.tsx
+++ src/components/DigitalPageComponents/useStepper.tsx
@@ -2,48 +2,40 @@ import { digitalPrintDiscountSteps, digitalPrintDiscountValue } from "../../Cons
 
 
 const useStepper = (numberOfCopy,price)=>{
-    //let discount:number = null
+
     let discountPrice = price
+    const arr = []
+    const priceLength = 250
+    const start = 90
+    const end = 40
 
-    const priceLength = 500
-    for (  let i = 0;i++; i<500 ){
-        let counter =+ 1;
-        console.log("counter", counter);
+    arr.push(start)
+    const diff = start - end
+    const discount = diff/priceLength
+    for (  let i = 0;i<priceLength; i++ ){
+        let res = arr[arr.length-1]- discount
+        arr.push(res)
     }
-    const discountHandler =(numberOfCopy)=>{
+    console.log(arr);
 
-            if(numberOfCopy===1){
-                return discountPrice = price
-            }
-            if(numberOfCopy=>digitalPrintDiscountSteps.copies_2 && numberOfCopy<=digitalPrintDiscountSteps.copies_4){
-                discountPrice  = price - (price*digitalPrintDiscountValue.stage_1)/100
-            }
-            if(numberOfCopy>digitalPrintDiscountSteps.copies_4 && numberOfCopy<=digitalPrintDiscountSteps.copies_6){
-                discountPrice = price - (price*digitalPrintDiscountValue.stage_2)/100
-                const priceLength = 500
-                const start = 100
-                const end = 40
-                const arr = []
-                arr.push(start)
-                const diff = start - end
-                const discount = diff/priceLength
-                for (  let i = 0;i<priceLength; i++ ){
-                     let res = arr[arr.length-1]- discount
-                     arr.push(res)
-                    // for(let j = 0;j<arr.length; j++){
-                    //
-                    // }
+    const discountHandler =(numberOfCopy)=> {
 
-                    // console.log("counter", counter);
-                }
-                console.log(arr);
+        if (numberOfCopy === 1) {
+            return discountPrice = price
+        }
+        if (numberOfCopy => digitalPrintDiscountSteps.copies_2 && numberOfCopy <= digitalPrintDiscountSteps.copies_4) {
+            discountPrice = price - (price * digitalPrintDiscountValue.stage_1) / 100
+        }
+        if (numberOfCopy > digitalPrintDiscountSteps.copies_4 && numberOfCopy <= digitalPrintDiscountSteps.copies_6) {
+            discountPrice = price - (price * digitalPrintDiscountValue.stage_2) / 100
 
-            }
-
+        }
+        if (numberOfCopy > digitalPrintDiscountSteps.copies_6) {
+           // discountPrice = price - (price * digitalPrintDiscountValue.stage_2) / 100
+            discountPrice = arr[numberOfCopy]
+        }
 
     }
-
-
 
     discountHandler(numberOfCopy)
     
