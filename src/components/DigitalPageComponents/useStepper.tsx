9413fa8e5bbaa73113cde3994a562e849692838d
Aleksander <fardmail@rambler.ru>
2023-01-28 12:27:15 +0300

    fist range price array

--- src/components/DigitalPageComponents/useStepper.tsx
+++ src/components/DigitalPageComponents/useStepper.tsx
@@ -4,36 +4,66 @@ import { digitalPrintDiscountSteps, digitalPrintDiscountValue } from "../../Cons
 const useStepper = (numberOfCopy,price)=>{
 
     let discountPrice = price
-    const arr = []
-    const priceLength = 250
-    const start = 90
-    const end = 40
+    /////////////////////////
 
-    arr.push(start)
-    const diff = start - end
-    const discount = diff/priceLength
-    for (  let i = 0;i<priceLength; i++ ){
-        let res = arr[arr.length-1]- discount
-        arr.push(res)
+    const arr = []
+
+    const priceLength = 250
+    const startPrice = 100
+    const endPrice = 40
+
+    const firstPriceRangeEnd = startPrice * 0.65
+    const firstPriceArray = []
+    const firstPriceRangeDiff = startPrice - firstPriceRangeEnd
+    const firstPriceRangeLength = 9
+    const firstDiscountValue = Number((firstPriceRangeDiff / firstPriceRangeLength).toFixed())
+    firstPriceArray.push(startPrice)
+    for (let i = 0; i<firstPriceRangeLength;i++){
+        let result = firstPriceArray[firstPriceArray.length-1] - firstDiscountValue
+        firstPriceArray.push(result)
     }
-    console.log(arr);
+    console.log(firstPriceArray);
+
+    // arr.push(start)
+    // const diff = start - end
+    // const discount = diff/priceLength
+    // for (  let i = 0;i<priceLength; i++ ){
+    //     let res = arr[arr.length-1]- discount
+    //     arr.push(res)
+    // }
+    // console.log(arr);
 
     const discountHandler =(numberOfCopy)=> {
 
-        if (numberOfCopy === 1) {
-            return discountPrice = price
+        if (numberOfCopy <= firstPriceRangeLength ) {
+            discountPrice = firstPriceArray[numberOfCopy-1]
         }
-        if (numberOfCopy => digitalPrintDiscountSteps.copies_2 && numberOfCopy <= digitalPrintDiscountSteps.copies_4) {
-            discountPrice = price - (price * digitalPrintDiscountValue.stage_1) / 100
-        }
-        if (numberOfCopy > digitalPrintDiscountSteps.copies_4 && numberOfCopy <= digitalPrintDiscountSteps.copies_6) {
-            discountPrice = price - (price * digitalPrintDiscountValue.stage_2) / 100
+        // if (numberOfCopy => digitalPrintDiscountSteps.copies_2 && numberOfCopy <= digitalPrintDiscountSteps.copies_4) {
+        //     discountPrice = price - (price * digitalPrintDiscountValue.stage_1) / 100
+        // }
+        // if (numberOfCopy > digitalPrintDiscountSteps.copies_4 && numberOfCopy <= digitalPrintDiscountSteps.copies_6) {
+        //     discountPrice = price - (price * digitalPrintDiscountValue.stage_2) / 100
+        //
+        // }
+        // if (numberOfCopy > digitalPrintDiscountSteps.copies_6) {
+        //     // discountPrice = price - (price * digitalPrintDiscountValue.stage_2) / 100
+        //     discountPrice = arr[numberOfCopy]
+        // }
 
-        }
-        if (numberOfCopy > digitalPrintDiscountSteps.copies_6) {
-           // discountPrice = price - (price * digitalPrintDiscountValue.stage_2) / 100
-            discountPrice = arr[numberOfCopy]
-        }
+        // if (numberOfCopy === 1) {
+        //     return discountPrice = price
+        // }
+        // if (numberOfCopy => digitalPrintDiscountSteps.copies_2 && numberOfCopy <= digitalPrintDiscountSteps.copies_4) {
+        //     discountPrice = price - (price * digitalPrintDiscountValue.stage_1) / 100
+        // }
+        // if (numberOfCopy > digitalPrintDiscountSteps.copies_4 && numberOfCopy <= digitalPrintDiscountSteps.copies_6) {
+        //     discountPrice = price - (price * digitalPrintDiscountValue.stage_2) / 100
+        //
+        // }
+        // if (numberOfCopy > digitalPrintDiscountSteps.copies_6) {
+        //    // discountPrice = price - (price * digitalPrintDiscountValue.stage_2) / 100
+        //     discountPrice = arr[numberOfCopy]
+        // }
 
     }
 
